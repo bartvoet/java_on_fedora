be61c7c04c2fc754605930d5aff5c0e988610958
bart <bart@localhost.localdomain>
2015-12-10 10:30:25 +0100

    initial projecte

---
+++ helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+    public class HelloWorldExtended {
+            public static void main(String[] args) {
+                    Hello hello = new Hello();
+                    World world = new World();
+                    hello.saySomething();
+                    System.out.print();
+                    world.saySomething();
+                    System.out.println();
+            }
+    }
